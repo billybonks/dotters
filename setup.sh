26db29750cf4e52476df5223488190b434857df6
billybonks <sebastienstettler@gmail.com>
2020-04-30 20:39:04 +0800

    update files

--- setup.sh
+++ setup.sh
@@ -71,6 +71,31 @@ else
   echo 'Redis already installed. Skipping'
 fi
 
+if  which heroku > /dev/null; then
+  echo 'Installing heroku cli'
+  brew tap heroku/brew && brew install heroku
+else
+  echo 'heroku cli already installed. Skipping'
+fi
+
+# To use the Heroku CLI's autocomplete --
+#   Via homebrew's shell completion:
+#     1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
+#         NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
+#               and called, either explicitly or via a framework like oh-my-zsh.
+#     2) Then run
+#       $ heroku autocomplete --refresh-cache
+#
+#   OR
+#
+#   Use our standalone setup:
+#     1) Run and follow the install steps:
+#       $ heroku autocomplete
+#
+# Bash completion has been installed to:
+#   /usr/local/etc/bash_completion.d
+
+
 if which nvm > /dev/null; then
   echo 'nvm already installed. Skipping'
 else
